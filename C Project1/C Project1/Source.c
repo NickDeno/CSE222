79eb79d57e16e7dfe619814b42a3cda491ea0e71
Nick DeNobrega <nickdenobrega@gmail.com>
2023-04-27 14:36:08 -0400

    Updated Source.c

--- C Project1/C Project1/Source.c
+++ C Project1/C Project1/Source.c
@@ -5,17 +5,9 @@ int main(void) {
 	int x = 25;
 	int y = 50;
 	int z = 75;
-
 	mySwapFunction(x, y);
-	
 	int test = MY_CONST1;
 	printf("Constant value: %d\n", test);
-
 	printf("The greatest number between %d, %d, and %d is %d", x, y, z, findGreatest(x, y, z));
-
-	//Defining array
-	int arr[5];
-	
-
 	return 0;
 }
